3b116df17ceaee754cd7e64917602aa3aed114c1
Spyros Ligouras <ligouras@admin.grnet.gr>
2024-04-27 01:58:52 +0300

    Add rotate func

--- tiling.py
+++ tiling.py
@@ -28,16 +28,32 @@ def place_tile(grid, x, y, gap, color):
     return grid
 
 
+def rotate(pos, gap):
+    if pos == gap:
+        return gap
+    if pos == NW:
+        return SE
+    elif pos == NE:
+        return SW
+    elif pos == SE:
+        return NW
+    elif pos == SW:
+        return NE
+
+
 def tile(grid, n, x=0, y=0, gap=NW):
     if n == 1:
         grid = place_tile(grid, x, y, gap, COLORS[1])
-    if n == 2:
-        grid = place_tile(grid, x + 1, y + 1, NW, COLORS[1])
+    elif n == 2:
+        grid = place_tile(grid, x + 1, y + 1, gap, COLORS[1])
 
-        grid = place_tile(grid, x, y, NW, COLORS[2])
-        grid = place_tile(grid, x + 2, y, SW, COLORS[0])
-        grid = place_tile(grid, x + 2, y + 2, NW, COLORS[2])
-        grid = place_tile(grid, x, y + 2, NE, COLORS[0])
+        grid = place_tile(grid, x, y, rotate(NW, gap), COLORS[2])
+        grid = place_tile(grid, x + 2, y, rotate(NE, gap), COLORS[0])
+        grid = place_tile(grid, x + 2, y + 2, rotate(SE, gap), COLORS[2])
+        grid = place_tile(grid, x, y + 2, rotate(SW, gap), COLORS[0])
+    else:
+        center = 2 ** (n - 1) - 1
+        grid = place_tile(grid, center, center, gap, COLORS[1])
 
     return grid
 
